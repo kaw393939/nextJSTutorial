1dfc924978c978943baed14be6a33da6e5fc4700
Nidhi Mishra <113460614+Nidhiiiim@users.noreply.github.com>
2023-07-13 01:03:23 -0400

    Updated repository with latest changes

--- tests/project.spec.js
+++ tests/project.spec.js
@@ -1,21 +1,23 @@
 const { test, expect } = require('@playwright/test');
-const nextjstest = 'http://localhost:3000'; // Replace with the URL of the student's resume page
-const nextjstestssr = 'http://localhost:3000/posts/ssg-ssr'
-const nextjsrender = 'http://localhost:3000/posts/pre-rendering'
-/* This test checks if `Blog` is present in the header */
+//CHange to localhost on which it is running on
+const nextjstest = 'http://localhost:3001/'; 
+const nextjstestssr = 'http://localhost:3001/posts/ssg-ssr'
+const nextjsrender = 'http://localhost:3001/posts/pre-rendering'
+
+// Test to check word 'Blog' in the Header
 test('Check Header for `name`', async ({ page }) => {
   await page.goto(nextjstest);
   const headerText = await page.textContent('h2');
   await expect(headerText).toContain('Blog');
 });
 
-/* This test checks if a yourname is present */
-test('Check Constant Presence', async () => {
+// Test checks if a yourname is present
+test('Check Yourname Presence on Home', async () => {
   const name = 'Yourname';
   expect(name).toBeDefined();
 });
-
-test('Check Header for Your Name on SSR page', async ({ page }) => {
+//Test to check Your name of SSR Page
+test('Check Header for YourName on SSR page', async ({ page }) => {
   await page.goto(nextjstestssr);
 
   const name = 'Yourname';
@@ -23,7 +25,7 @@ test('Check Header for Your Name on SSR page', async ({ page }) => {
   await expect(page.locator(headerSelector)).toBeVisible();
 });
 
-/* This test checks if the <h1> in the <article> contains specific content */
+// This test checks if the <h1> in the <article> in SSR page contains specific content 
 test('Check <h1> in <article> for Specific Content on SSR page', async ({ page }) => {
   await page.goto(nextjstestssr);
 
@@ -33,6 +35,7 @@ test('Check <h1> in <article> for Specific Content on SSR page', async ({ page }
 
   await expect(page.locator(headerSelector)).toBeVisible();
 });
+//Test to check for yourname on render page header
 test('Check Header for Your Name on Render Page', async ({ page }) => {
   await page.goto(nextjsrender);
 
@@ -41,7 +44,7 @@ test('Check Header for Your Name on Render Page', async ({ page }) => {
   await expect(page.locator(headerSelector)).toBeVisible();
 });
 
-/* This test checks if the <h1> in the <article> contains specific content */
+//This test checks if the <h1> in the <article> of Pre-Rendering Page contains specific content
 test('Check <h1> in <article> for Specific Content on Render Page', async ({ page }) => {
   await page.goto(nextjsrender);
 
@@ -51,33 +54,55 @@ test('Check <h1> in <article> for Specific Content on Render Page', async ({ pag
 
   await expect(page.locator(headerSelector)).toBeVisible();
 });
-/* This test checks that the responsive meta tag is present */
+// This test checks that the responsive meta tag is present 
 test('Check Responsive Meta Tag', async ({ page }) => {
   await page.goto(nextjsrender);
   const viewportMeta = await page.getAttribute('meta[name="viewport"]', 'content');
   await expect(viewportMeta).toBe('width=device-width');
 });
 
-/* This test checks that the responsive meta tag is present */
+// This test checks that the responsive meta tag is present 
 test('Check Responsive Meta Tag for Render', async ({ page }) => {
   await page.goto(nextjstestssr);
   const viewportMeta = await page.getAttribute('meta[name="viewport"]', 'content');
   await expect(viewportMeta).toBe('width=device-width');
 });
 
-/* This test checks for correct redirection on clicking an <a> element */
-test('Check Redirection on Click', async ({ page }) => {
+//Test for checking Click for SSR page navigation
+test('Test for SSR Navigator', async ({ page }) => {
+  await page.goto(nextjstest);
+  await page.getByText('When to Use Static Generation v.s. Server-side Rendering').click();
+  await page.waitForURL(nextjstestssr);
+  const currentURL = await page.url();
+  const expectedURL = nextjstestssr;
+  expect(currentURL).toBe(expectedURL);
+});
+
+//Test to check Click for Pre-Rendering Navigation
+test('Test for Pre-Rendering Navigator', async ({ page }) => {
+  await page.goto(nextjstest);
+  await page.getByText('Two Forms of Pre-rendering').click();
+  await page.waitForURL(nextjsrender);
+  const currentURL = await page.url();
+  const expectedURL = nextjsrender;
+  expect(currentURL).toBe(expectedURL);
+});
+//Test to Check Back Home from SSR page Navigation 
+test('Test for SSR Back Home Nav', async ({ page }) => {
   await page.goto(nextjstestssr);
+  await page.getByText('Back to home').click();
+  await page.waitForURL(nextjstest);
+  const currentURL = await page.url();
+  const expectedURL = nextjstest;
+  expect(currentURL).toBe(expectedURL);
+});
 
-  const expectedURL = 'http://localhost:3000/';
-
-  // Click the <a> element with the text "back to home"
-  await Promise.all([
-    page.waitForNavigation({ timeout: 10000 }), // Increased timeout to 10 seconds
-    page.click('a:has-text("back to home")', { timeout: 10000 }), // Increased timeout to 10 seconds
-  ]);
-
-  const currentURL = page.url();
-
+//Test to Check Back Home from Pre-Rendering page Navigation
+test('Test for Pre-Rendering Back Home Nav', async ({ page }) => {
+  await page.goto(nextjsrender);
+  await page.getByText('Back to home').click();
+  await page.waitForURL(nextjstest);
+  const currentURL = await page.url();
+  const expectedURL = nextjstest;
   expect(currentURL).toBe(expectedURL);
 });
